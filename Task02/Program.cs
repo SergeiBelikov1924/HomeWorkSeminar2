ceab78f6e35aae292955303d984ac6a26c4a7845
Кот <Sergei Belikov>
2023-09-29 12:21:02 +0700

    es ist commit

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -3,6 +3,7 @@
 //a = 5; b = 7 -> max = 7
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
+
 int nA, nB;
 Console.WriteLine("Введите первое число: ");
 nA=Convert.ToInt32(Console.ReadLine());
---
+++ Task04a/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 4:
+// Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+int nA;
+int nB;
+int nC;
+Console.WriteLine("Введите первое число: ",nA);
+nA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ",nB);
+nB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ",nC);
+nC = Convert.ToInt32(Console.ReadLine());
+
+if ((nA>nB) && (nA>nC))
+{
+    Console.WriteLine("Наибольшее число:",nA);
+}
+else if ((nB>nA) && (nB>nC))
+{
+    Console.WriteLine("Наибольшее число:",nB);
+}
+else if (((nC>nA) && (nC>nB)));
+{
+    Console.WriteLine("Наибольшее число:",nC);
+}
