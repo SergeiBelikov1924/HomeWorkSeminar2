e09cc2befb9ef10ffaa6b4ecf4749c0dc61da2ad
Кот <Sergei Belikov>
2023-10-05 21:18:15 +0700

    HomeWork for Seminar4

---
+++ Task27/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число
+//  и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// int SumNumbers (int num);
+// {
+//     int sum = 0;
+
+//     for (int i = 1; i <= num; i++)
+//     {
+//         checked
+//         {
+//             sum += i; // sum = sum + i
+//         }
+//     }
+
+//     return sum;
+// }
+
+
+// using System;
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.Write("n = ");
+//         var n = Convert.ToInt32(Console.ReadLine());
+//         var sum = 0;
+//         while(n > 0)
+//         {
+//             sum += n % 10;
+//             n /= 10;
+//         }
+//         Console.WriteLine(sum);
+//     }
+// }
+
+Console.WriteLine("Введите целое положительное число:");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int num = 1;
+
+while (i > 0)
+    {
+        num = i % 10;
+        i = i/10;
+        sum = sum + num;
+    }
+
+Console.WriteLine($"Сумма всех цифр в числе равна: " + sum);
---
+++ Task29/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 29: Напишите программу, 
+// которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] RandomArray (int length)
+{
+    int[] arr = new int [length];
+    var rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = rnd.Next();
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int[] array = RandomArray(8);
+
+PrintArray(array);
+
