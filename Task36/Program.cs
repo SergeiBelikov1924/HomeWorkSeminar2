f70b1378ac6616ba6f53cd02b7722e8b6d2f54c7
Кот <Sergei Belikov>
2023-10-13 10:24:54 +0700

    HomeWork Seminar5

---
+++ Task36/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 36: Задайте одномерный массив, 
+// заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+
+ArrayRandomNumbers(numbers);
+Console.Write("[ ");
+PrintArray(numbers);
+Console.WriteLine("]");
+
+int SumNumbersNotEvenIndex = 0;
+for (int i = 1; i < numbers.Length; i += 2)
+{
+    SumNumbersNotEvenIndex += numbers [i];
+}
+
+Console.WriteLine($"Сумма элементов на нечётных позициях: {SumNumbersNotEvenIndex}");
+
+void ArrayRandomNumbers(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100,100);
+    }
+}
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
---
+++ Task38/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+Console.WriteLine("Введите размер массива:");
+
+double[] CreateArreyRndDouble(int size, int min, int max)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.NextDouble() * (max - min) + min;
+    }
+    return arr;
+}
+
+
+void PrintArrayDouble(double[] arr)
+{
+    Console.WriteLine("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        //double nRound = Math.Round(arr[i], 1);
+        if (i < arr.Length - 1) Console.Write($"{arr[i]:F1}, ");
+        else Console.Write($"{arr[i]:F1}");
+    }
+    Console.WriteLine("]");
+}
